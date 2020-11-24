20f5c71147bdfe644438a851a9a72f4e0cd706e8
Tyler Thompson <tyler.keith.thompson@gmail.com>
2020-11-24 10:37:13 -0700

    [orechestration-engine] - Clean up unneeded packages and told fastlane not to do a pod install anymore - TT

--- Workflow/Package.swift
+++ Workflow/Package.swift
@@ -5,11 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Workflow",
-    platforms: [
-        .iOS(.v12)
-    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Workflow",
             targets: ["Workflow"]),
@@ -18,16 +14,9 @@ let package = Package(
             targets: ["WorkflowDI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: Version("2.0.0-beta.1")),
-        .package(url: "https://github.com/mattgallagher/CwlCatchException.git", from: Version("2.0.0-beta.1")),
-        .package(url: "https://github.com/nallick/UIUTest.git", from: Version("1.7.0")),
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.7.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Workflow",
             dependencies: []),
@@ -40,8 +29,6 @@ let package = Package(
             dependencies: [
                 "Workflow",
                 "WorkflowDI",
-                "CwlPreconditionTesting",
-                "CwlCatchException",
             ],
             exclude: ["Info.plist"]),
     ]
