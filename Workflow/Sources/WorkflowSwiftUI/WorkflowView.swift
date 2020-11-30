e3e220fc26ae5364e998c8365eadf218dbdfd528
Tyler Thompson <tyler.keith.thompson@gmail.com>
2020-11-30 07:08:52 -0700

    [orechestration-engine] - I am on to something here with regards to knowing when it was dismissed...but It is just out of reach

--- Workflow/Sources/WorkflowSwiftUI/WorkflowView.swift
+++ Workflow/Sources/WorkflowSwiftUI/WorkflowView.swift
@@ -8,15 +8,34 @@
 import Foundation
 import SwiftUI
 import Workflow
+import Combine
+
 @available(iOS 14.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension FlowRepresentable where Self: View {
     var _workflowUnderlyingInstance:Any { AnyView(self) }
 }
 
 @available(iOS 14.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public class Holder: ObservableObject {
+    @Published var state:Bool = true
+
+    let view:AnyView
+
+    private var subscribers: Set<AnyCancellable> = []
+
+    init(view: AnyView) {
+        self.view = view
+        $state.sink { (newVal) in
+            print(newVal)
+            print("")
+        }.store(in: &subscribers)
+    }
+}
+
+@available(iOS 14.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public class WorkflowModel: ObservableObject, AnyOrchestrationResponder {
     @Published var view:AnyView = AnyView(EmptyView())
-    var stack = LinkedList<AnyView>()
+    var stack = LinkedList<Holder>()
 
     var launchStyle:LaunchStyle.PresentationType = .default
 
@@ -24,28 +43,22 @@ public class WorkflowModel: ObservableObject, AnyOrchestrationResponder {
         guard let view = to.instance.value?.underlyingInstance as? AnyView else { return }
         launchStyle = LaunchStyle.PresentationType(rawValue: to.metadata.launchStyle) ?? .default
         self.view = view
-        stack.append(view)
+        stack.append(Holder(view: view))
     }
 
     public func proceed(to: (instance: AnyWorkflow.InstanceNode, metadata: FlowRepresentableMetaData), from: (instance: AnyWorkflow.InstanceNode, metadata: FlowRepresentableMetaData)) {
         guard let next = to.instance.value?.underlyingInstance as? AnyView else { return }
-        //first
-//        let current = cur
-//        var v = AnyView(EmptyView())
-//        if let back1 = prev {
-//            v = AnyView(Wrapper(next: next, current: current).environmentObject(self))
-//            if let _ = prevPrev {
-//                v = AnyView(Wrapper(next: v, current: back1).environmentObject(self))
-//            }
-//        }
-        stack.append(next)
+        stack.append(Holder(view: next))
         var v = next
         _ = stack.last?.traverse(direction: .backward, until: {
-            let current = $0.value
-            v = AnyView(Wrapper(next: v, current: current).environmentObject(self))
+            if !$0.value.state {
+                $0.value.state.toggle()
+            } else {
+                v = AnyView(Wrapper(next: v, current: $0.value.view).environmentObject(self).environmentObject($0.value))
+            }
             return false
         })
-        view = v//AnyView(Wrapper(next: next, current: view).environmentObject(self))
+        view = v
     }
 
     public func proceedBackward(from: (instance: AnyWorkflow.InstanceNode, metadata: FlowRepresentableMetaData), to: (instance: AnyWorkflow.InstanceNode, metadata: FlowRepresentableMetaData)) {
@@ -60,18 +73,14 @@ public class WorkflowModel: ObservableObject, AnyOrchestrationResponder {
 @available(iOS 14.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 struct Wrapper: View {
     @EnvironmentObject var model: WorkflowModel
+    @EnvironmentObject var holder: Holder
     let next: AnyView
     let current: AnyView
 
-    @State var showingModal = true
-
     var body: some View {
-        current.sheet(isPresented: $showingModal, content: {
+        current.sheet(isPresented: $holder.state, content: {
             next
         })
-//        .onChange(of: showingModal, perform: { _ in
-//            model.stack.removeLast()
-//        })
     }
 }
 
