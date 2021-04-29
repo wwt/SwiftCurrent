2f43c92e0a63e90194b6c1eb497fe353bf859a09
Heather Meadow <heather.meadow@wwt.com>
2021-04-29 12:38:35 -0600

    Fixed tests so WorkflowTests does not reference UIUTest - HM RAG TT

--- Workflow/Tests/WorkflowTests/CustomAssertions.swift
+++ Workflow/Tests/WorkflowTests/CustomAssertions.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import XCTest
-import UIUTest
 
 @testable import CwlPreconditionTesting
 
@@ -20,17 +19,4 @@ func XCTAssertThrowsFatalError(instructions: @escaping () -> Void, file: StaticS
     }
     XCTAssertNotNil(exception, "No fatal error thrown", file: file, line: line)
     XCTAssertFalse(reached, "Code executed past expected fatal error", file: file, line: line)
-}
-
-func XCTAssertUIViewControllerDisplayed<T: UIViewController>(ofType viewControllerType: T.Type, file: StaticString = #file, line: UInt = #line) {
-    waitUntil(UIApplication.topViewController() is T)
-    waitUntil(UIApplication.topViewController()?.view.willRespondToUser == true)
-    XCTAssert(UIApplication.topViewController() is T, "Expected top view controller to be \(T.self) but was: \(String(describing: UIApplication.topViewController()))", file: file, line: line)
-}
-
-func XCTAssertUIViewControllerDisplayed<T: UIViewController>(isInstance viewController: T, file: StaticString = #file, line: UInt = #line) {
-    waitUntil(UIApplication.topViewController() is T)
-    waitUntil(UIApplication.topViewController()?.view.willRespondToUser == true)
-    XCTAssert(UIApplication.topViewController() is T, "Expected top view controller to be \(T.self) but was: \(String(describing: UIApplication.topViewController()))", file: file, line: line)
-    XCTAssert(UIApplication.topViewController() === viewController, "Expected top view controller to be instance \(viewController) but was: \(String(describing: UIApplication.topViewController()))", file: file, line: line)
 }
