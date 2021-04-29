5b6fa952a646a88133ba9fb0f659a53435f67612
Tyler Thompson <tyler.thompson@wwt.com>
2021-04-29 16:27:40 -0600

    [orchestration-engine] - Fixed some more of the copyright headers - TT

--- WorkflowExample/WorkflowUIKitTests/ModalStyleTests.swift
+++ WorkflowExample/WorkflowUIKitTests/ModalStyleTests.swift
@@ -3,7 +3,7 @@
 //  WorkflowTests
 //
 //  Created by Tyler Thompson on 3/8/20.
-//  Copyright © 2020 Tyler Thompson. All rights reserved.
+//  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
 //
 
 import Foundation
