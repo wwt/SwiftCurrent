de89c9bae4d0a2fa5466b1b5eb4d204b1b5899bd
Richard Gist <Richard.Gist@wwt.com>
2021-05-12 17:54:04 -0600

    [orchestration-engine] - updated docs in LinkedListComparable - TT RAG

--- Workflow/Sources/Workflow/LinkedList/LinkedListComparable.swift
+++ Workflow/Sources/Workflow/LinkedList/LinkedListComparable.swift
@@ -10,34 +10,25 @@
 
 import Foundation
 extension LinkedList where Value: Comparable {
-    /// Sorts the linked list in place using a merge sort
+    /// Sorts the linked list in place using a merge sort.
     public func sort() {
         guard first?.next != nil else { return }
         first = LinkedList(mergeSort(first) { $0 <= $1 }).first
     }
 
-    /**
-    A non-mutating sort method
-    - Returns: A sorted version of the LinkedList
-    */
+    /// Returns a sorted version of the linked list.
     public func sorted() -> LinkedList<Value> {
         LinkedList(mergeSort(first) { $0 <= $1 })
     }
 
-    /**
-    Returns the maximum value in the comparable LinkedList
-    - Returns: The maximum concrete value in the LinkedList or nil if there is none
-    */
+    /// Returns the maximum concrete value in the linked list; nil if there is none.
     public func max() -> Value? {
         guard var max = first?.value else { return nil }
         forEach { max = Swift.max(max, $0.value) }
         return max
     }
 
-    /**
-    Returns the minimum value in the comparable LinkedList
-    - Returns: The minimum concrete value in the LinkedList or nil if there is none
-    */
+    /// Returns the minimum concrete value in the linked list; nil if there is none.
     public func min() -> Value? {
         guard var min = first?.value else { return nil }
         forEach { min = Swift.min(min, $0.value) }
