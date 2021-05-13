54563df78b0112bc134cf7a60869ba05b50ceb25
Richard Gist <Richard.Gist@wwt.com>
2021-05-13 11:02:30 -0600

    [orchestration-engine] - Adding deprecation file with some initial deprecations to improve upgrade path - TT RAG

---
+++ Workflow/Sources/Workflow/Deprecations/WorkflowDeprecations.swift
@@ -0,0 +1,26 @@
+//  swiftlint:disable:this file_name
+//  WorkflowDeprecations.swift
+//  Workflow
+//
+//  Created by Richard Gist on 5/13/21.
+//  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
+//  swiftlint:disable all
+
+import Foundation
+
+extension Workflow {
+    @available(*, unavailable, renamed: "thenPresent(_:presentationType:flowPersistence:)")
+    public func thenPresent<F>(_ type:F.Type, presentationType:LaunchStyle = LaunchStyle.default, staysInViewStack:@escaping @autoclosure () -> FlowPersistence) -> Workflow where F: FlowRepresentable {
+        fatalError("Obsoleted")
+    }
+
+    @available(*, unavailable, renamed: "thenPresent(_:presentationType:flowPersistence:)")
+    public func thenPresent<F>(_ type:F.Type, presentationType:LaunchStyle = LaunchStyle.default, staysInViewStack:@escaping (F.WorkflowInput) -> FlowPersistence) -> Workflow where F: FlowRepresentable {
+        fatalError("Obsoleted")
+    }
+
+    @available(*, unavailable, renamed: "thenPresent(_:presentationType:flowPersistence:)")
+    public func thenPresent<F>(_ type:F.Type, presentationType:LaunchStyle = LaunchStyle.default, staysInViewStack:@escaping () -> FlowPersistence) -> Workflow where F: FlowRepresentable, F.WorkflowInput == Never {
+        fatalError("Obsoleted")
+    }
+}
