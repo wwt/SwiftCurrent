b0b231da15497337213dac5d6c875fd6ccf5c1e3
Tyler Thompson <tyler.thompson@wwt.com>
2021-06-14 19:38:24 -0600

    [rename] - There are already too many files for anybody to reasonably read, so lets just add more and generate the docs - TT

--- document.sh
+++ document.sh
@@ -1,6 +1,6 @@
 SDK_PATH=`xcrun --sdk iphonesimulator --show-sdk-path`
-sourcekitten doc --spm --module-name Workflow -- -Xswiftc "-sdk" -Xswiftc "$SDK_PATH" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios14.0-simulator" > swiftcurrent-docs.json
-sourcekitten doc --module-name WorkflowUIKit -- -workspace ./SwiftCurrent.xcworkspace -scheme SwiftCurrent_UIKit -destination "platform=iOS Simulator,name=iPhone 12" > swiftcurrentuikit-docs.json
+sourcekitten doc --spm --module-name SwiftCurrent -- -Xswiftc "-sdk" -Xswiftc "$SDK_PATH" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios14.0-simulator" > swiftcurrent-docs.json
+sourcekitten doc --module-name SwiftCurrent_UIKit -- -workspace ./SwiftCurrent.xcworkspace -scheme SwiftCurrent_UIKit -destination "platform=iOS Simulator,name=iPhone 12" > swiftcurrentuikit-docs.json
 jazzy --sourcekitten-sourcefile swiftcurrent-docs.json,swiftcurrentuikit-docs.json
 rm swiftcurrent-docs.json
 rm swiftcurrentuikit-docs.json
