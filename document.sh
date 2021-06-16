70c8d96d3b822e6c8ec45b7226b9ec204345cbab
Tyler Thompson <tyler.thompson@wwt.com>
2021-06-16 16:26:00 -0600

    [jazzy-update2] - Updated jazzy docs, it now uses the correct version number too! - TT RAG

--- document.sh
+++ document.sh
@@ -1,7 +1,7 @@
 SDK_PATH=`xcrun --sdk iphonesimulator --show-sdk-path`
 sourcekitten doc --spm --module-name SwiftCurrent -- -Xswiftc "-sdk" -Xswiftc "$SDK_PATH" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios14.0-simulator" > swiftcurrent-docs.json
 sourcekitten doc --module-name SwiftCurrent_UIKit -- -workspace ./SwiftCurrent.xcworkspace -scheme SwiftCurrent_UIKit -destination "platform=iOS Simulator,name=iPhone 12" > swiftcurrentuikit-docs.json
-jazzy --sourcekitten-sourcefile swiftcurrent-docs.json,swiftcurrentuikit-docs.json
+jazzy --podspec SwiftCurrent.podspec --sourcekitten-sourcefile swiftcurrent-docs.json,swiftcurrentuikit-docs.json
 rm swiftcurrent-docs.json
 rm swiftcurrentuikit-docs.json
 open docs/index.html
