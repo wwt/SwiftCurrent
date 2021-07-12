6d4260b5d074351393f077c4963b5268cbd17da2
Tyler Thompson <tyler.thompson@wwt.com>
2021-07-12 17:26:50 -0600

    [swiftui] - Added tests to SwiftUI package - TT

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ let package = Package(
         .package(url: "https://github.com/mattgallagher/CwlCatchException.git", from: Version("2.0.0-beta.1")),
         .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/sindresorhus/ExceptionCatcher", from: "2.0.0"),
+        .package(url: "https://github.com/nalexn/ViewInspector.git", from: "0.8.1")
     ],
     targets: [
         .target(
@@ -43,5 +44,13 @@ let package = Package(
                 .product(name: "Algorithms", package: "swift-algorithms")
             ],
             exclude: ["Info.plist", "SwiftCurrent.xctestplan"]),
+        .testTarget(
+            name: "SwiftCurrent_SwiftUITests",
+            dependencies: [
+                "SwiftCurrent",
+                "CwlPreconditionTesting",
+                "CwlCatchException",
+                "ViewInspector"
+            ]),
     ]
 )
---
+++ Tests/SwiftCurrent_SwiftUITests/SwiftCurrent_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCurrent_SwiftUITests.swift
+//  SwiftCurrent
+//
+//  Created by Tyler Thompson on 7/12/21.
+//  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
+//
+
+import XCTest
+
+@testable import SwiftCurrent
+
+final class SwiftCurrent_SwiftUITests: XCTestCase {
+    func testMethod() {
+        XCTAssert(true)
+    }
+}
