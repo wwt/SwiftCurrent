145ef73a2c6fed5110d9ebdf9cd29233b54ecf78
Tyler Thompson <tyler.thompson@wwt.com>
2021-07-12 18:26:19 -0600

    [swiftui] - Updated documentation generation to include new symbols from SwiftCurrent_SwiftUI - TT

--- .github/document.sh
+++ .github/document.sh
@@ -1,7 +1,9 @@
 SDK_PATH=`xcrun --sdk iphonesimulator --show-sdk-path`
 sourcekitten doc --spm --module-name SwiftCurrent -- -Xswiftc "-sdk" -Xswiftc "$SDK_PATH" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios14.0-simulator" > swiftcurrent-docs.json
 sourcekitten doc --module-name SwiftCurrent_UIKit -- -workspace ./SwiftCurrent.xcworkspace -scheme SwiftCurrent_UIKit -destination "platform=iOS Simulator,name=iPhone 12" > swiftcurrentuikit-docs.json
-jazzy --config .github/.jazzy.yaml --podspec SwiftCurrent.podspec --sourcekitten-sourcefile swiftcurrent-docs.json,swiftcurrentuikit-docs.json
+sourcekitten doc --spm --module-name SwiftCurrent_SwiftUI -- -Xswiftc "-sdk" -Xswiftc "$SDK_PATH" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios14.0-simulator" > swiftcurrent-swiftui-docs.json
+jazzy --config .github/.jazzy.yaml --podspec SwiftCurrent.podspec --sourcekitten-sourcefile swiftcurrent-docs.json,swiftcurrentuikit-docs.json,swiftcurrent-swiftui-docs.json
 rm swiftcurrent-docs.json
 rm swiftcurrentuikit-docs.json
+rm swiftcurrent-swiftui-docs.json
 open docs/index.html
