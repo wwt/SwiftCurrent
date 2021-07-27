638ded1027ee67923c7af61e20a37f3c601d28ab
Richard Gist <Richard.Gist@wwt.com>
2021-07-27 13:53:52 -0600

    [main] - Adding timeout enum to SwiftUI tests to help with Inspect call taking too long in the pipeline - RAG BL
    Co-authored-by: Richard Gist <Richard.Gist@wwt.com>
    Co-authored-by: Brian Lombardo <lombardb@wwt.com>

---
+++ Tests/SwiftCurrent_SwiftUITests/TestConstant.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Richard Gist on 7/27/21.
+//
+
+import Foundation
+
+enum TestConstant {
+    static var timeout: TimeInterval {
+        5.0 // Pipeline
+//        return 0.1; #warning("Local timeout should not be checked in")
+    }
+}
