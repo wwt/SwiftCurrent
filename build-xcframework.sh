d90990a1d0927be48d2c3c10aaf6afabbd29f05b
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2021-08-07 21:54:26 -0600

    [xcframework-builder] - added a sort of hacky build xcframework script to get usable xcframework files - TT

---
+++ build-xcframework.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# CREDIT: https://forums.swift.org/t/how-to-build-swift-package-as-xcframework/41414/4
+# NOTE: This does modify the Package.swift file, which is less than ideal. 
+set -x
+set -e
+
+# Pass scheme name as the first argument to the script
+NAME=$1
+
+# Build the scheme for all platforms that we plan to support
+for PLATFORM in "iOS" "iOS Simulator"; do
+
+    case $PLATFORM in
+    "iOS")
+    RELEASE_FOLDER="Release-iphoneos"
+    ;;
+    "iOS Simulator")
+    RELEASE_FOLDER="Release-iphonesimulator"
+    ;;
+    esac
+
+    ARCHIVE_PATH=$RELEASE_FOLDER
+
+    # Rewrite Package.swift so that it declaras dynamic libraries, since the approach does not work with static libraries
+    perl -i -p0e 's/type: .static,//g' Package.swift
+    perl -i -p0e 's/type: .dynamic,//g' Package.swift
+    perl -i -p0e 's/BETA_//g' Package.swift
+    perl -i -p0e 's/(library[^,]*,)/$1 type: .dynamic,/g' Package.swift
+
+    xcodebuild archive -workspace . -scheme $NAME \
+            -destination "generic/platform=$PLATFORM" \
+            -archivePath $ARCHIVE_PATH \
+            -derivedDataPath ".build" \
+            SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+    FRAMEWORK_PATH="$ARCHIVE_PATH.xcarchive/Products/usr/local/lib/$NAME.framework"
+    MODULES_PATH="$FRAMEWORK_PATH/Modules"
+    mkdir -p $MODULES_PATH
+
+    BUILD_PRODUCTS_PATH=".build/Build/Intermediates.noindex/ArchiveIntermediates/$NAME/BuildProductsPath"
+    RELEASE_PATH="$BUILD_PRODUCTS_PATH/$RELEASE_FOLDER"
+    SWIFT_MODULE_PATH="$RELEASE_PATH/$NAME.swiftmodule"
+    RESOURCES_BUNDLE_PATH="$RELEASE_PATH/${NAME}_${NAME}.bundle"
+
+    # Copy Swift modules
+    if [ -d $SWIFT_MODULE_PATH ] 
+    then
+        cp -r $SWIFT_MODULE_PATH $MODULES_PATH
+    else
+        # In case there are no modules, assume C/ObjC library and create module map
+        echo "module $NAME { export * }" > $MODULES_PATH/module.modulemap
+        # TODO: Copy headers
+    fi
+
+    # Copy resources bundle, if exists 
+    if [ -e $RESOURCES_BUNDLE_PATH ] 
+    then
+        cp -r $RESOURCES_BUNDLE_PATH $FRAMEWORK_PATH
+    fi
+
+done
+
+xcodebuild -create-xcframework \
+-framework Release-iphoneos.xcarchive/Products/usr/local/lib/$NAME.framework \
+-framework Release-iphonesimulator.xcarchive/Products/usr/local/lib/$NAME.framework \
+-output $NAME.xcframework
+
+# .library(
+#             name: "SwiftCurrent", type: .dynamic,
+#             targets: ["SwiftCurrent"]),
+#         .library(
+#             name: "SwiftCurrent_UIKit", type: .dynamic,
+#             targets: ["SwiftCurrent_UIKit"]),
+#         .library(
+#             name: "BETA_SwiftCurrent_SwiftUI", type: .dynamic,
+#             targets: ["SwiftCurrent_SwiftUI"])
