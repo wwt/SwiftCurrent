eec720a8c1ee8611a1e13f1500777307d9a50f45
Tyler Thompson <tyler.thompson@wwt.com>
2021-08-17 14:30:11 -0600

    [testing-package] - Change XCTest check to a runtime check, still only post in if debug builds - TT

--- Sources/SwiftCurrent/TestOnly/TestOnly.swift
+++ Sources/SwiftCurrent/TestOnly/TestOnly.swift
@@ -13,7 +13,7 @@ import Foundation
 
 @available(iOS 11.0, macOS 10.14, tvOS 13, watchOS 7.4, *)
 extension Notification.Name {
-    #if DEBUG && canImport(XCTest)
+    #if DEBUG
     /// :nodoc: A notification only available when tests are being run that lets you know a workflow has been launched.
     public static var workflowLaunched: Notification.Name {
         .init(rawValue: "WorkflowLaunched")
@@ -33,8 +33,9 @@ enum EventReceiver {
                                  args: AnyWorkflow.PassedArgs,
                                  style: LaunchStyle,
                                  onFinish: ((AnyWorkflow.PassedArgs) -> Void)?) {
+        #if DEBUG
         if #available(iOS 11.0, macOS 10.14, tvOS 13, watchOS 7.4, *) {
-            #if DEBUG && canImport(XCTest)
+            guard NSClassFromString("XCTest") != nil else { return }
             NotificationCenter.default.post(name: .workflowLaunched, object: [
                 "workflow": workflow,
                 "responder": responder,
@@ -42,13 +43,14 @@ enum EventReceiver {
                 "style": style,
                 "onFinish": onFinish as Any
             ])
-            #endif
         }
+        #endif
     }
 
     static func flowRepresentableMetadataCreated<F: FlowRepresentable>(metadata: FlowRepresentableMetadata, type: F.Type) {
         if #available(iOS 11.0, macOS 10.14, tvOS 13, watchOS 7.4, *) {
-            #if DEBUG && canImport(XCTest)
+            #if DEBUG
+            guard NSClassFromString("XCTest") != nil else { return }
             NotificationCenter.default.post(name: .flowRepresentableMetadataCreated, object: [
                 "metadata": metadata,
                 "type": String(describing: type)
