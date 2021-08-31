ca5c2ea50523cf65c3dbffb8f61a0e073b33aea7
Morgan Zellers <morgan.zellers@gmail.com>
2021-08-31 13:03:46 -0500

    [skip ci] swiftui-modals - Beginning to add tests for modals - TT MZ
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>

---
+++ Sources/SwiftCurrent_SwiftUI/ViewInspector/InspectableSheet.swift
@@ -0,0 +1,35 @@
+//
+//  InspectableSheet.swift
+//  InspectableSheet
+//
+//  Created by Morgan Zellers on 8/31/21.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+extension View {
+    func testableSheet<Sheet>(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, @ViewBuilder content: @escaping () -> Sheet
+    ) -> some View where Sheet: View {
+        return self.modifier(InspectableSheet(isPresented: isPresented, onDismiss: onDismiss, content: content))
+    }
+}
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+struct InspectableSheet<Sheet>: ViewModifier where Sheet: View {
+    let isPresented: Binding<Bool>
+    let onDismiss: (() -> Void)?
+    let content: () -> Sheet
+    let sheetBuilder: () -> Any
+
+    init(isPresented: Binding<Bool>, onDismiss: (() -> Void)?, content: @escaping () -> Sheet) {
+        self.isPresented = isPresented
+        self.onDismiss = onDismiss
+        self.content = content
+        sheetBuilder = { content() as Any }
+    }
+
+    func body(content: Self.Content) -> some View {
+        content.sheet(isPresented: isPresented, content: self.content)
+    }
+}
--- Tests/SwiftCurrent_SwiftUITests/ViewInspector/InspectableExtensions.swift
+++ Tests/SwiftCurrent_SwiftUITests/ViewInspector/InspectableExtensions.swift
@@ -19,3 +19,6 @@ extension WorkflowLauncher: Inspectable { }
 
 @available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
 extension Inspection: InspectionEmissary where V: View { }
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+extension InspectableSheet: SheetProvider { }
