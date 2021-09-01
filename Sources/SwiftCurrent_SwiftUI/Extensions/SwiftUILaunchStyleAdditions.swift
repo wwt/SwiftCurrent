91a69c1f1c0ea2c09739e45cc558aa1751a95862
Richard Gist <Richard.Gist@wwt.com>
2021-09-01 14:48:22 -0600

    [fullScreenCover] - Adding ModalPresentationStyle to SwiftUI - TT RAG
    Co-authored-by: Tyler Thompson <tyler.thompson@wwt.com>

--- Sources/SwiftCurrent_SwiftUI/Extensions/SwiftUILaunchStyleAdditions.swift
+++ Sources/SwiftCurrent_SwiftUI/Extensions/SwiftUILaunchStyleAdditions.swift
@@ -19,38 +19,48 @@ extension LaunchStyle {
     public enum SwiftUI {
         /// A type indicating how a `FlowRepresentable` should be presented.
         public enum PresentationType: RawRepresentable, CaseIterable {
+            public static let allCases: [LaunchStyle.SwiftUI.PresentationType] = [.default, .navigationLink, .modal(.sheet), .modal(.fullScreenCover)]
+
             /**
-            Indicates a `FlowRepresentable` can be launched contextually.
-            - Important: This swaps out SwiftUI Views and does not animate by default; you can supply your own animations.
-            */
+             Indicates a `FlowRepresentable` can be launched contextually.
+             - Important: This swaps out SwiftUI Views and does not animate by default; you can supply your own animations.
+             */
             case `default`
 
             /**
-            Indicates a `FlowRepresentable` should be wrapped in a NavigationLink.
-            - Important: You are responsible for supplying a NavigationView.
-            */
+             Indicates a `FlowRepresentable` should be wrapped in a NavigationLink.
+             - Important: You are responsible for supplying a NavigationView.
+             */
             case navigationLink
 
             /**
-            Indicates a `FlowRepresentable` should be presented modally.
-            - Important: Will not effect the first item in a Workflow.
-            */
-            case modal
+             Indicates a `FlowRepresentable` should be presented modally.
+             - Important: Will not effect the first item in a Workflow.
+             */
+            case modal(ModalPresentationStyle = .sheet)
 
             /// Creates a `PresentationType` from a `LaunchStyle`, or returns nil if no mapping exists.
             public init?(rawValue: LaunchStyle) {
                 switch rawValue {
                     case .default: self = .default
                     case ._swiftUI_navigationLink: self = .navigationLink
-                    case ._swiftUI_modal: self = .modal
+                    case ._swiftUI_modal: self = .modal()
+                    case ._swiftUI_modal_fullscreen: self = .modal(.fullScreenCover)
                     default: return nil
                 }
             }
+
+            /**
+             Indicates a `FlowRepresentable` should be presented modally.
+             - Important: Will not effect the first item in a Workflow.
+             */
+            public static let modal = Self.modal()
 
             /// The corresponding `LaunchStyle` for this `PresentationType`
             public var rawValue: LaunchStyle {
                 switch self {
                     case .navigationLink: return ._swiftUI_navigationLink
+                    case .modal(.fullScreenCover): return ._swiftUI_modal_fullscreen
                     case .modal: return ._swiftUI_modal
                     case .default: return .default
                 }
@@ -63,5 +73,15 @@ extension LaunchStyle.SwiftUI.PresentationType: Equatable {
     /// :nodoc: Equatable protocol requirement.
     public static func == (lhs: LaunchStyle.SwiftUI.PresentationType, rhs: LaunchStyle.SwiftUI.PresentationType) -> Bool {
         lhs.rawValue === rhs.rawValue
+    }
+}
+
+extension LaunchStyle.SwiftUI {
+    /// Modal presentation styles available when presenting sheets
+    public enum ModalPresentationStyle {
+        /// Presents a sheet
+        case sheet
+        /// Presents a modal view that covers as much of the screen as possible
+        case fullScreenCover
     }
 }
