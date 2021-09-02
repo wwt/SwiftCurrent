a2762daa50d6f3dc7b2ddbadef5c85713bdc8466
Richard Gist <Richard.Gist@wwt.com>
2021-09-02 09:32:55 -0600

    [fullScreenCover] - Addiing availability to tests so they compile - RAG

--- Tests/SwiftCurrent_SwiftUITests/GenericConstraintTests.swift
+++ Tests/SwiftCurrent_SwiftUITests/GenericConstraintTests.swift
@@ -14,6 +14,7 @@ import SwiftCurrent
 
 @testable import SwiftCurrent_SwiftUI
 
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
 extension FlowRepresentable {
     var persistence: FlowPersistence? {
         workflow?.first { item in
