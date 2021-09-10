104754778bba747e489b96e50a14dda638c24147
Tyler Thompson <tyler.thompson@wwt.com>
2021-09-10 11:08:53 -0600

    [new-docs] - Replace readme video with vimeo embed - TT

--- .github/DocsPostProcessor/Sources/DocsPostProcessor/DocsPostProcessor.swift
+++ .github/DocsPostProcessor/Sources/DocsPostProcessor/DocsPostProcessor.swift
@@ -10,6 +10,9 @@ struct DocsPostProcessor: ParsableCommand {
     @Flag(help: "Replaces overview in the left navigation with rendered README")
     var replaceOverviewWithReadme = false
 
+    @Flag(help: "Replaces readme video link with an embedded vimeo player")
+    var replaceReadmeVideoWithVimeoEmbed = false
+
     @Argument(help: "The file, or directory with the HTML to change")
     var path: String
 
@@ -28,8 +31,12 @@ struct DocsPostProcessor: ParsableCommand {
             let html = try String(contentsOf: file, encoding: .utf8)
             let doc: Document = try SwiftSoup.parse(html)
             if replaceOverviewWithReadme {
-                print("Replacing overview with README for: \(file.absoluteString)")
                 try replaceOverviewWithReadme(document: doc)
+                print("Replaced overview with README for: \(file.absoluteString)")
+            }
+            if replaceReadmeVideoWithVimeoEmbed, file.lastPathComponent == "index.html" {
+                try replaceReadmeVideoWithVimeoEmbed(document: doc)
+                print("Replaced readme video with vimeo embed")
             }
             try doc.html().write(to: file, atomically: true, encoding: .utf8)
         }
@@ -43,6 +50,15 @@ struct DocsPostProcessor: ParsableCommand {
             } else {
                 throw Err.noOverviewFound
             }
+        }
+    }
+
+    func replaceReadmeVideoWithVimeoEmbed(document: Document) throws {
+        if let article = try? document.select("article").first() {
+            let link = try article.getElementsByAttributeValue("href", "https://user-images.githubusercontent.com/33705774/132767762-7447753c-feba-4ef4-b54c-38bfe9d1ee82.mp4")
+            try link.wrap("""
+            <iframe src="https://player.vimeo.com/video/600610695?h=fd3976b77a" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            """)
         }
     }
 }
