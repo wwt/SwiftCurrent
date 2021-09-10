0d101c4a058058177748fd967fa0f612327def8b
Tyler Thompson <tyler.thompson@wwt.com>
2021-09-10 13:33:25 -0600

    [refactors] - Added a comment pointing to the SO answer that shed light on the nav link issue - TT

--- Sources/SwiftCurrent_SwiftUI/Views/NavigationWrapper.swift
+++ Sources/SwiftCurrent_SwiftUI/Views/NavigationWrapper.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 @available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
 extension View {
+    // PROBLEM: SwiftUI has a bug if isActive is defaulted to true on NavLinks.
+    // See details here: https://stackoverflow.com/questions/68365774/nested-navigationlinks-with-isactive-true-are-not-displaying-correctly
     func navLink<D: View>(to destination: D, isActive: Binding<Bool>) -> some View {
         background(
             List {
