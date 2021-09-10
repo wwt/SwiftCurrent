b22c74b86a5b5f25733200c74af57e0e6ad39e8a
Tyler Thompson <tyler.thompson@wwt.com>
2021-09-10 10:57:13 -0600

    [new-docs] - Added a post processor for modifying jazzy docs as we might desire - TT

---
+++ .github/DocsPostProcessor/Sources/DocsPostProcessor/DocsPostProcessor.swift
@@ -0,0 +1,48 @@
+import Foundation
+import ArgumentParser
+import SwiftSoup
+
+struct DocsPostProcessor: ParsableCommand {
+    enum Err: Error {
+        case noOverviewFound
+    }
+
+    @Flag(help: "Replaces overview in the left navigation with rendered README")
+    var replaceOverviewWithReadme = false
+
+    @Argument(help: "The file, or directory with the HTML to change")
+    var path: String
+
+    mutating func run() throws {
+        let url = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(path)
+        var files: [URL] = (url.pathExtension == "html") ? [url] : []
+        if url.pathExtension != "html", let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles]) {
+            for case let fileURL as URL in enumerator {
+                let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                if fileAttributes.isRegularFile == true, fileURL.pathExtension == "html" {
+                    files.append(fileURL)
+                }
+            }
+        }
+        for file in files {
+            let html = try String(contentsOf: file, encoding: .utf8)
+            let doc: Document = try SwiftSoup.parse(html)
+            if replaceOverviewWithReadme {
+                print("Replacing overview with README for: \(file.absoluteString)")
+                try replaceOverviewWithReadme(document: doc)
+            }
+            try doc.html().write(to: file, atomically: true, encoding: .utf8)
+        }
+    }
+
+    func replaceOverviewWithReadme(document: Document) throws {
+        if let nav = try? document.select("nav").first() {
+            if let firstNavLink = try nav.getElementsByClass("nav-group-name-link").first(),
+               URL(fileURLWithPath: try firstNavLink.attr("href")).lastPathComponent == "Overview.html" {
+                try firstNavLink.attr("href", "index.html")
+            } else {
+                throw Err.noOverviewFound
+            }
+        }
+    }
+}
---
+++ .github/DocsPostProcessor/Sources/DocsPostProcessor/main.swift
@@ -0,0 +1,1 @@
+DocsPostProcessor.main()
