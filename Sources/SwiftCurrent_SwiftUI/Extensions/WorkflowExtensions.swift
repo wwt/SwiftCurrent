e53ec90995be01b7eac71c50d56bc482b066926e
Richard Gist <Richard.Gist@wwt.com>
2021-11-10 12:29:51 -0700

    [launch-with-workflow] - Adding missing init that was causing tests to fail - RAG

--- Sources/SwiftCurrent_SwiftUI/Extensions/WorkflowExtensions.swift
+++ Sources/SwiftCurrent_SwiftUI/Extensions/WorkflowExtensions.swift
@@ -42,6 +42,19 @@ extension Workflow where F: FlowRepresentable & View {
         })
     }
 
+    /**
+     Creates a `Workflow` with a `FlowRepresentable`.
+     - Parameter type: a reference to the first `FlowRepresentable`'s concrete type in the workflow.
+     - Parameter launchStyle: the `LaunchStyle` the `FlowRepresentable` should use while it's part of this workflow.
+     - Parameter flowPersistence: a closure returning a `FlowPersistence` representing how this item in the workflow should persist.
+     */
+    public convenience init(_ type: F.Type,
+                            launchStyle: LaunchStyle = .default,
+                            flowPersistence: @escaping () -> FlowPersistence) where F.WorkflowInput == Never {
+        self.init(ExtendedFlowRepresentableMetadata(flowRepresentableType: type,
+                                                    launchStyle: launchStyle) { _ in flowPersistence() })
+    }
+
     /// Called when the workflow should be terminated, and the app should return to the point before the workflow was launched.
     public func abandon() {
         AnyWorkflow(self).abandon()
