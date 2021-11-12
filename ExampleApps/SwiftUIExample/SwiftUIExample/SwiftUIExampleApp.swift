7d5acb3e18de39188817f5a1197b487930c417ad
Richard Gist <Richard.Gist@wwt.com>
2021-11-12 10:55:13 -0700

    [skip ci] data-spike4 - This spike takes the registry a bit farther than 3 where we can now generate workflows from strings - RAG

--- ExampleApps/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
+++ ExampleApps/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -12,8 +12,22 @@ import SwiftCurrent_SwiftUI
 
 @main
 struct SwiftUIExampleApp: App {
+    let startingWorkflow: AnyWorkflow
+
     init() {
         Container.default.register(UserDefaults.self) { _ in UserDefaults.standard }
+
+        DataDriven.shared.register(ExtendedFlowRepresentableMetadata(flowRepresentableType: SwiftCurrentOnboarding.self), for: "SwiftCurrentOnboarding")
+        DataDriven.shared.register(key: ContentView.self, creating: ExtendedFlowRepresentableMetadata(flowRepresentableType: ContentView.self))
+        DataDriven.register(type: LoginView.self)
+        print(DataDriven.shared.registryDescription)
+
+        do {
+            startingWorkflow = try DataDriven.shared.getWorkflow(from: ["SwiftCurrentOnboarding", "ContentView"])
+        } catch {
+            let defaultWorkflow = Workflow(ContentView.self)
+            startingWorkflow = AnyWorkflow(defaultWorkflow)
+        }
     }
 
     var body: some Scene {
@@ -21,14 +35,75 @@ struct SwiftUIExampleApp: App {
             if Environment.shouldTest {
                 TestView()
             } else {
-                WorkflowLauncher(isLaunched: .constant(true)) {
-                    thenProceed(with: SwiftCurrentOnboarding.self) {
-                        thenProceed(with: ContentView.self)
-                            .applyModifiers { $0.transition(.slide) }
-                    }.applyModifiers { $0.transition(.slide) }
-                }
+//                WorkflowLauncher(isLaunched: .constant(true)) {
+//                    thenProceed(with: SwiftCurrentOnboarding.self) {
+//                        thenProceed(with: ContentView.self)
+//                            .applyModifiers { $0.transition(.slide) }
+//                    }.applyModifiers { $0.transition(.slide) }
+//                }
+                WorkflowLauncher(isLaunched: .constant(true), workflow: startingWorkflow)
                 .preferredColorScheme(.dark)
             }
         }
+    }
+}
+
+import SwiftCurrent
+/// Manages ``FlowRepresentable`` types that will be driven through data.
+open class DataDriven {
+    // I don't like this.
+    static let shared = DataDriven()
+
+    private var registry1 = [String: ExtendedFlowRepresentableMetadata]()
+
+    // I'm leaning towards this being the preferred registry.
+    private var registry2 = [String: () -> ExtendedFlowRepresentableMetadata]()
+
+    /// Current human readable description of the registry.
+    public var registryDescription: String {
+        var stringy = "Registry contains:\n"
+        for thisKey in registry1.keys {
+            stringy += "  - key: \"\(thisKey)\" : \(registry1[thisKey]!.underlyingTypeDescription)\n"
+        }
+        return stringy
+    }
+
+    func register(_ efrm: @escaping @autoclosure () -> ExtendedFlowRepresentableMetadata, for key: String) {
+        registry1[key] = efrm()
+        registry2[key] = efrm
+    }
+
+    func register(key: Any, creating efrm: @escaping @autoclosure () -> ExtendedFlowRepresentableMetadata) {
+        let key = String(describing: key)
+        print("Registering key: \(key)")
+        registry1[key] = efrm()
+        registry2[key] = efrm
+    }
+
+    /// Registers the provided type in the data driven registry.
+    public class func register<FR: FlowRepresentable & View>(type: FR.Type) {
+        let key = String(describing: type)
+        let closure = { return ExtendedFlowRepresentableMetadata(flowRepresentableType: type) }
+
+        // This thing could be a instance method that doesn't go directly to shared.  Maybe it could take in shared, I'm not sure.
+        shared.registry1[key] = closure()
+        shared.registry2[key] = closure
+    }
+
+    func getWorkflow(from types: [String]) throws -> AnyWorkflow {
+        let workflow = AnyWorkflow.empty
+        for thing in types {
+            if let efrm = registry2[thing] {
+                workflow.append(efrm())
+            } else {
+                throw Error.unregisteredType
+            }
+        }
+
+        return workflow
+    }
+
+    enum Error: Swift.Error {
+        case unregisteredType
     }
 }
--- Sources/SwiftCurrent_SwiftUI/Models/ExtendedFlowRepresentableMetadata.swift
+++ Sources/SwiftCurrent_SwiftUI/Models/ExtendedFlowRepresentableMetadata.swift
@@ -10,31 +10,38 @@ import SwiftUI
 import SwiftCurrent
 
 @available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
-class ExtendedFlowRepresentableMetadata: FlowRepresentableMetadata {
+public class ExtendedFlowRepresentableMetadata: FlowRepresentableMetadata {
     private(set) var workflowItemFactory: (AnyWorkflowItem?) -> AnyWorkflowItem
 
-    init<FR: FlowRepresentable & View>(flowRepresentableType: FR.Type,
-                                       launchStyle: LaunchStyle = .default,
-                                       flowPersistence: @escaping (AnyWorkflow.PassedArgs) -> FlowPersistence,
-                                       flowRepresentableFactory: @escaping (AnyWorkflow.PassedArgs) -> AnyFlowRepresentable) {
+    public init<FR: FlowRepresentable & View>(flowRepresentableType: FR.Type,
+                                              launchStyle: LaunchStyle = .default,
+                                              flowPersistence: @escaping (AnyWorkflow.PassedArgs) -> FlowPersistence = { _ in .default },
+                                              flowRepresentableFactory: @escaping (AnyWorkflow.PassedArgs) -> AnyFlowRepresentable) {
         workflowItemFactory = {
             guard let wrappedWorkflowItem = $0 else { return AnyWorkflowItem(view: WorkflowItem(FR.self)) }
             return AnyWorkflowItem(view: WorkflowItem(FR.self) { wrappedWorkflowItem })
         }
+
+        underlyingTypeDescription = String(describing: flowRepresentableType)
 
         super.init(flowRepresentableType, launchStyle: launchStyle, flowPersistence: flowPersistence, flowRepresentableFactory: flowRepresentableFactory)
     }
 
-    init<FR: FlowRepresentable & View>(flowRepresentableType: FR.Type,
-                                       launchStyle: LaunchStyle = .default,
-                                       flowPersistence: @escaping (AnyWorkflow.PassedArgs) -> FlowPersistence) {
+    public init<FR: FlowRepresentable & View>(flowRepresentableType: FR.Type,
+                                              launchStyle: LaunchStyle = .default,
+                                              flowPersistence: @escaping (AnyWorkflow.PassedArgs) -> FlowPersistence = { _ in .default }) {
         workflowItemFactory = {
             guard let wrappedWorkflowItem = $0 else { return AnyWorkflowItem(view: WorkflowItem(FR.self)) }
             return AnyWorkflowItem(view: WorkflowItem(FR.self) { wrappedWorkflowItem })
         }
+
+        underlyingTypeDescription = String(describing: flowRepresentableType)
 
         super.init(flowRepresentableType, launchStyle: launchStyle, flowPersistence: flowPersistence) { args in
             AnyFlowRepresentableView(type: FR.self, args: args)
         }
     }
+
+    /// The type name for the underlying ``FlowRepresentable``
+    public let underlyingTypeDescription: String
 }
