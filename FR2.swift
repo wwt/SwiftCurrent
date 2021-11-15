540ebb5ebd61114797f667e9cd30cf609b6c74e8
Morgan Zellers <morgan.zellers@gmail.com>
2021-11-15 15:13:52 -0600

    Spiked out a script to parse output from SourceKitten's structure call.

---
+++ FR2.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+import SwiftCurrent_SwiftUI
+
+struct FR2: View, FlowRepresentable, Inspectable {
+    var _workflowPointer: AnyFlowRepresentable?
+    var body: some View { Text("FR2 type") }
+}
---
+++ findFlowRepresentables.swift
@@ -0,0 +1,109 @@
+/*
+    usage:
+        xcrun --sdk macosx swiftc -parse-as-library findFlowRepresentables.swift -o FindFlowRepresentables 
+        sourcekitten structure --file FR1.swift | ./FindFlowRepresentables
+*/
+
+import Foundation
+
+@main
+struct FindFlowRepresentables {
+    static func main() throws {
+        // for argument in CommandLine.arguments {
+        //     print("\(argument)")
+        // }
+
+        let json = readSTDIN() as! String
+
+        var frFiles: [String] = []
+        guard let file = try? JSONDecoder().decode(File.self, from: json.data(using: .utf8)!) else { print("Oops"); throw fatalError() }
+
+        if let substructure = file.keySubstructure.first,
+           let containsFlowRep = substructure.keyInheritedtypes?.contains(where: { $0.keyName == "FlowRepresentable" }),
+           containsFlowRep == true {
+            frFiles.append(substructure.keyName)
+        }
+
+        frFiles.forEach { print($0) }
+    }
+}
+
+// MARK: - File
+struct File: Codable {
+    let keyDiagnosticStage: String
+    let keyLength, keyOffset: Int
+    let keySubstructure: [KeySubstructure]
+
+    enum CodingKeys: String, CodingKey {
+        case keyDiagnosticStage = "key.diagnostic_stage"
+        case keyLength = "key.length"
+        case keyOffset = "key.offset"
+        case keySubstructure = "key.substructure"
+    }
+}
+
+// MARK: - KeySubstructure
+struct KeySubstructure: Codable {
+    let keyAccessibility: String?
+    let keyBodylength, keyBodyoffset: Int?
+    let keyElements: [KeyElement]?
+    let keyInheritedtypes: [KeyInheritedtype]?
+    let keyKind: String
+    let keyLength: Int
+    let keyName: String
+    let keyNamelength, keyNameoffset, keyOffset: Int
+    let keySubstructure: [KeySubstructure]?
+    let keySetterAccessibility, keyTypename: String?
+
+    enum CodingKeys: String, CodingKey {
+        case keyAccessibility = "key.accessibility"
+        case keyBodylength = "key.bodylength"
+        case keyBodyoffset = "key.bodyoffset"
+        case keyElements = "key.elements"
+        case keyInheritedtypes = "key.inheritedtypes"
+        case keyKind = "key.kind"
+        case keyLength = "key.length"
+        case keyName = "key.name"
+        case keyNamelength = "key.namelength"
+        case keyNameoffset = "key.nameoffset"
+        case keyOffset = "key.offset"
+        case keySubstructure = "key.substructure"
+        case keySetterAccessibility = "key.setter_accessibility"
+        case keyTypename = "key.typename"
+    }
+}
+
+// MARK: - KeyElement
+struct KeyElement: Codable {
+    let keyKind: String
+    let keyLength, keyOffset: Int
+
+    enum CodingKeys: String, CodingKey {
+        case keyKind = "key.kind"
+        case keyLength = "key.length"
+        case keyOffset = "key.offset"
+    }
+}
+
+// MARK: - KeyInheritedtype
+struct KeyInheritedtype: Codable {
+    let keyName: String
+
+    enum CodingKeys: String, CodingKey {
+        case keyName = "key.name"
+    }
+}
+
+func readSTDIN () -> String? {
+  var input: String?
+
+  while let line = readLine() {
+      if input == nil {
+          input = line
+      } else {
+          input! += "\n" + line
+      }
+  }
+
+  return input
+}
