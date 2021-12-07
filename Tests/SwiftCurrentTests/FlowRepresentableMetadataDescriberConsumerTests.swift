652b8c6de22f053c439998c6c0e29ea2a9eed073
Richard Gist <Richard.Gist@wwt.com>
2021-12-07 12:20:18 -0700

    [skip ci] metadata-describer - This has tests to showcase/test extending SwiftCurrent but it does not behave as one would expect with classes - RAG

--- Tests/SwiftCurrentTests/FlowRepresentableMetadataDescriberConsumerTests.swift
+++ Tests/SwiftCurrentTests/FlowRepresentableMetadataDescriberConsumerTests.swift
@@ -33,7 +33,79 @@ class FlowRepresentableMetadataDescriberConsumerTests: XCTestCase {
 
         let FRMD = FR2.self as FlowRepresentableMetadataDescriber.Type
 
-        XCTAssertEqual(FRMD?.flowRepresentableName, "FR2")
-        XCTAssertEqual(FRMD?.createMetadata().flowRepresentableTypeDescriptor, "FR2")
+        XCTAssertEqual(FRMD.flowRepresentableName, "FR2")
+        XCTAssertEqual(FRMD.createMetadata().flowRepresentableTypeDescriptor, "FR2")
+    }
+
+    func testExtendingProductsCanProvideUniqueImplementationsForClasses() {
+        class ThirdMetada: FlowRepresentableMetadata { }
+        class FR1: CustomExtensionClass { }
+        class FR2: CustomExtensionClass {
+            static var flowRepresentableName: String { "Special FR2"}
+            static func createMetadata() -> FlowRepresentableMetadata {
+                ThirdMetada(Self.self) { _ in .default }
+            }
+        }
+
+        let FRMD1 = FR1.self as FlowRepresentableMetadataDescriber.Type
+        let FRMD2 = FR2.self as FlowRepresentableMetadataDescriber.Type
+
+        XCTAssertEqual(FR1.flowRepresentableName, "Twice Overridden")
+        XCTAssert(FR1.createMetadata() is CustomFlowRepresentableMetadata)
+        XCTAssertEqual(FRMD1.flowRepresentableName, "Twice Overridden")
+        XCTAssert(FRMD1.createMetadata() is CustomFlowRepresentableMetadata)
+
+        XCTAssertEqual(FR2.flowRepresentableName, "Special FR2")
+        XCTAssert(FR2.createMetadata() is ThirdMetada)
+        XCTAssertEqual(FRMD2.flowRepresentableName, "Special FR2")
+        XCTAssert(FRMD2.createMetadata() is ThirdMetada)
+    }
+
+    func testExtendingProductsCanProvideUniqueImplementationsForStructs() {
+        class ThirdMetada: FlowRepresentableMetadata { }
+        struct FR1: CustomExtensionProtocol {
+            var _workflowPointer: AnyFlowRepresentable?
+        }
+        struct FR2: CustomExtensionProtocol {
+            var _workflowPointer: AnyFlowRepresentable?
+
+            static var flowRepresentableName: String { "Special FR2"}
+            static func createMetadata() -> FlowRepresentableMetadata {
+                ThirdMetada(Self.self) { _ in .default }
+            }
+        }
+
+        let FRMD1 = FR1.self as FlowRepresentableMetadataDescriber.Type
+        let FRMD2 = FR2.self as FlowRepresentableMetadataDescriber.Type
+
+        XCTAssertEqual(FR1.flowRepresentableName, "Twice Overridden for Protocol")
+        XCTAssert(FR1.createMetadata() is CustomFlowRepresentableMetadata)
+        XCTAssertEqual(FRMD1.flowRepresentableName, "Twice Overridden for Protocol")
+        XCTAssert(FRMD1.createMetadata() is CustomFlowRepresentableMetadata)
+
+        XCTAssertEqual(FR2.flowRepresentableName, "Special FR2")
+        XCTAssert(FR2.createMetadata() is ThirdMetada)
+        XCTAssertEqual(FRMD2.flowRepresentableName, "Special FR2")
+        XCTAssert(FRMD2.createMetadata() is ThirdMetada)
+    }
+}
+
+fileprivate class CustomExtensionClass: FlowRepresentable {
+    var _workflowPointer: AnyFlowRepresentable?
+    required init() { }
+}
+fileprivate class CustomFlowRepresentableMetadata: FlowRepresentableMetadata { }
+fileprivate extension FlowRepresentable where Self: CustomExtensionClass {
+    static var flowRepresentableName: String { "Twice Overridden" }
+    static func createMetadata() -> FlowRepresentableMetadata {
+        CustomFlowRepresentableMetadata(Self.self) { _ in .default }
+    }
+}
+
+fileprivate protocol CustomExtensionProtocol: FlowRepresentable { }
+fileprivate extension FlowRepresentable where Self: CustomExtensionProtocol {
+    static var flowRepresentableName: String { "Twice Overridden for Protocol" }
+    static func createMetadata() -> FlowRepresentableMetadata {
+        CustomFlowRepresentableMetadata(Self.self) { _ in .default }
     }
 }
