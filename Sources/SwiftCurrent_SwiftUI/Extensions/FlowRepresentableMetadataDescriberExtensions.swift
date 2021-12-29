947f5f02999ae1dd91d2ef03b1843707a39aac92
Richard Gist <Richard.Gist@wwt.com>
2021-12-29 13:37:11 -0700

    [metadata-describer] - Added Default implementation for metadataFactory when Self is a View and FlowRepresentable - RAG

---
+++ Sources/SwiftCurrent_SwiftUI/Extensions/FlowRepresentableMetadataDescriberExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  FlowRepresentableMetadataDescriberExtensions.swift
+//  SwiftCurrent
+//
+//  Created by Richard Gist on 12/15/21.
+//  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
+//  
+
+import SwiftUI
+import SwiftCurrent
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+extension View where Self: FlowRepresentable & FlowRepresentableMetadataDescriber {
+    static func metadataFactory() -> FlowRepresentableMetadata {
+        ExtendedFlowRepresentableMetadata(flowRepresentableType: Self.self) { _ in .default }
+    }
+}
