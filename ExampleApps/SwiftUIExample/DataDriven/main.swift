2f57d765abbfb76d77b98d92188341e70a529ae0
Morgan Zellers <morgan.zellers@gmail.com>
2022-01-13 09:26:27 -0600

    Cleaning things up from previous approach

--- ExampleApps/SwiftUIExample/DataDriven/main.swift
+++ ExampleApps/SwiftUIExample/DataDriven/main.swift
@@ -10,16 +10,12 @@
 import Foundation
 import SwiftSyntax
 
-let SUBSTRUCTURE_KEY = "key.substructure"
-let NAME_KEY = "key.name"
-let INHERITEDTYPES_KEY = "key.inheritedtypes"
 var conformance = "FlowRepresentable"
 try main()
 
 func main() throws {
     let directoryPath = CommandLine.arguments[1]
     let fileURLs = getSwiftFileURLs(from: directoryPath)
-
     let files: [File] = fileURLs.compactMap { try? File(url: $0) }
 
     printFindings(files)
@@ -27,51 +23,12 @@ func main() throws {
 
 func printFindings(_ files: [File]) {
     files.forEach {
-        print("\($0.results.rootNode.types.first?.name)")
-    }
-
-    let classes = files.filter { $0.results.rootNode.types.first?.type == .class }
-
-    classes.forEach {
         guard let name = $0.results.rootNode.types.first?.name,
               let inheritence = $0.results.rootNode.types.first?.inheritance else { return }
 
-        print("Inheritance for \(name): \(inheritence)")
-    }
-}
-
-class ConformanceFinder: SyntaxRewriter {
-    var classAndStructNames: [String] = []
-    var extensionNames: [String] = []
-    var protocolNames: [String] = []
-
-    override func visit(_ token: TokenSyntax) -> Syntax {
-        let tokenIsStructOrClass: Bool = token.previousToken?.tokenKind == .structKeyword || token.previousToken?.tokenKind == .classKeyword
-        let tokenIsExtension: Bool = token.previousToken?.tokenKind == .extensionKeyword
-        let tokenIsProtocol: Bool = token.previousToken?.tokenKind == .protocolKeyword
-
-        if tokenIsExtension {
-            if let name = scanForConformance(token) { extensionNames.append(name) }
-        } else if tokenIsProtocol {
-            if let name = scanForConformance(token) { protocolNames.append(name) }
-        } else if tokenIsStructOrClass {
-            if let name = scanForConformance(token) { classAndStructNames.append(name) }
+        if inheritence.contains(conformance) {
+            print("Inheritance for \(name): \(inheritence)")
         }
-
-        return Syntax(token)
-    }
-
-    private func scanForConformance(_ token: TokenSyntax) -> String? {
-        var fileToken: TokenSyntax? = token
-
-        while fileToken?.text != "{" {
-            guard let currentToken = fileToken, !currentToken.tokenKind.isKeyword else { break }
-            if currentToken.text == conformance {
-                return token.text
-            }
-            fileToken = currentToken.nextToken
-        }
-        return nil
     }
 }
 
