ee446701972ca5efa6e322226edc6580d56e0e38
Tyler Thompson <tyler.thompson@wwt.com>
2022-03-21 13:19:58 -0600

    [data-driven-merge-on-5.0] - Nick did all the work, I will take the credit! - TT NK MF
    Co-authored-by: Nick Kaczmarek <Nick.Kaczmarek@wwt.com>
    Co-authored-by: Matt Freiburg <Matt.Freiburg@wwt.com>

--- Sources/SwiftCurrent_SwiftUI/TypeErased/AnyWorkflowItem.swift
+++ Sources/SwiftCurrent_SwiftUI/TypeErased/AnyWorkflowItem.swift
@@ -4,35 +4,82 @@
 //
 //  Created by Morgan Zellers on 11/2/21.
 //  Copyright © 2021 WWT and Tyler Thompson. All rights reserved.
-//  
+//
+//  swiftlint:disable private_over_fileprivate file_types_order
 
 import SwiftUI
 import SwiftCurrent
 
 @available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
 public struct AnyWorkflowItem: View, _WorkflowItemProtocol {
-    public var workflowLaunchStyle: LaunchStyle.SwiftUI.PresentationType {
-        .default
-    }
-
     public typealias FlowRepresentableType = Never
-
-    public func canDisplay(_ element: AnyWorkflow.Element?) -> Bool {
-        false
-    }
-
-    public func modify(workflow: AnyWorkflow) {
-
-    }
 
     let inspection = Inspection<Self>()
     private let _body: AnyView
+    private var storage: AnyWorkflowItemStorageBase
 
     public var body: some View {
         _body.onReceive(inspection.notice) { inspection.visit(self, $0) }
     }
 
     init<W: _WorkflowItemProtocol>(view: W) {
-        _body = AnyView(view)
+        var copy = view
+        storage = AnyWorkflowItemStorage(&copy)
+        _body = AnyView(copy)
+    }
+}
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+extension AnyWorkflowItem {
+    /// :nodoc: Protocol requirement.
+    public var workflowLaunchStyle: LaunchStyle.SwiftUI.PresentationType {
+        storage.workflowLaunchStyle
+    }
+
+    /// :nodoc: Protocol requirement.
+    public func canDisplay(_ element: AnyWorkflow.Element?) -> Bool {
+        storage.canDisplay(element)
+    }
+
+    /// :nodoc: Protocol requirement.
+    public func modify(workflow: AnyWorkflow) {
+        storage.modify(workflow: workflow)
+    }
+}
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+fileprivate class AnyWorkflowItemStorageBase {
+    // swiftlint:disable:next unavailable_function
+    func canDisplay(_ element: AnyWorkflow.Element?) -> Bool {
+        fatalError("AnyWorkflowItemStorageBase called directly, only available internally so something has gone VERY wrong.")
+    }
+
+    // swiftlint:disable:next unavailable_function
+    func modify(workflow: AnyWorkflow) {
+        fatalError("AnyWorkflowItemStorageBase called directly, only available internally so something has gone VERY wrong.")
+    }
+
+    var workflowLaunchStyle: LaunchStyle.SwiftUI.PresentationType {
+        fatalError("AnyWorkflowItemStorageBase called directly, only available internally so something has gone VERY wrong.")
+    }
+}
+
+@available(iOS 14.0, macOS 11, tvOS 14.0, watchOS 7.0, *)
+fileprivate final class AnyWorkflowItemStorage<Wrapped: _WorkflowItemProtocol>: AnyWorkflowItemStorageBase {
+    var holder: Wrapped
+    init(_ wrapped: inout Wrapped) {
+        holder = wrapped
+    }
+
+    override func canDisplay(_ element: AnyWorkflow.Element?) -> Bool {
+        holder.canDisplay(element)
+    }
+
+    override func modify(workflow: AnyWorkflow) {
+        holder.modify(workflow: workflow)
+    }
+
+    override var workflowLaunchStyle: LaunchStyle.SwiftUI.PresentationType {
+        holder.workflowLaunchStyle
     }
 }
