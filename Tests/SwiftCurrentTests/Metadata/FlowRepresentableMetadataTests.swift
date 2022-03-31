978a594186ee77588addd71b9d4035604e982b4c
Tyler Thompson <tyler.thompson@wwt.com>
2022-03-31 14:40:40 -0600

    [data-driven] - Remove code that was changed just for the sake of tests - TT

--- Tests/SwiftCurrentTests/Metadata/FlowRepresentableMetadataTests.swift
+++ Tests/SwiftCurrentTests/Metadata/FlowRepresentableMetadataTests.swift
@@ -9,6 +9,16 @@
 import XCTest
 @testable import SwiftCurrent
 
+extension FlowRepresentableMetadata {
+    convenience init<FR: FlowRepresentable>(_ flowRepresentableType: FR.Type) {
+        self.init(flowRepresentableType,
+                  launchStyle: .default,
+                  flowPersistence: { _ in .default }) { args in
+            AnyFlowRepresentable(FR.self, args: args)
+        }
+    }
+}
+
 class FlowRepresentableMetadataTests : XCTestCase {
     func testDefaultsUseDefaultProperty() {
         final class FR1: FlowRepresentable { var _workflowPointer: AnyFlowRepresentable? }
@@ -24,7 +34,7 @@ class FlowRepresentableMetadataTests : XCTestCase {
         final class FR2: FlowRepresentable { var _workflowPointer: AnyFlowRepresentable? }
         final class FR1: FlowRepresentable { var _workflowPointer: AnyFlowRepresentable? }
 
-        let actual = FlowRepresentableMetadata(FR1.self) { _ in
+        let actual = FlowRepresentableMetadata(FR1.self, launchStyle: .default, flowPersistence: { _ in .default }) { _ in
             AnyFlowRepresentable(FR2.self, args: .none)
         }
         _ = actual.setPersistence(.none)
