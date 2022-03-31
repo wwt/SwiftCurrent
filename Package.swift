dc70f28488ca3b3908b78247c48a27ac1b301339
Tyler Thompson <tyler.thompson@wwt.com>
2022-03-31 13:22:45 -0600

    [data-driven] - zhuzhed up our measurement tests, they are more accurate Swift code and guaranteed to be the same length with no collisions - TT

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: Version("2.0.0-beta.1")),
         .package(url: "https://github.com/mattgallagher/CwlCatchException.git", from: Version("2.0.0-beta.1")),
-        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/sindresorhus/ExceptionCatcher", from: "2.0.0"),
         .package(url: "https://github.com/nalexn/ViewInspector.git", from: "0.9.1"),
         .package(url: "https://github.com/apple/swift-argument-parser", .exact("1.0.3")), // For pipeline
